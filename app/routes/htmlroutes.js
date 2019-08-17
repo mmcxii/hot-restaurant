ab7440b344d9af78fb29386877e4b7cdfd0c01c4
Remeto <remetojohn@gmail.com>
2019-08-17 11:53:18 -0700

    deleted not used code

--- app/routes/htmlroutes.js
+++ app/routes/htmlroutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-
 const index = express.static(path.join(__dirname, '../public'));
 
 module.exports = index;
