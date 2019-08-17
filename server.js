ee43f52224cce8fb2505a50bd517fa1db36fa7cb
Remeto <remetojohn@gmail.com>
2019-08-17 11:51:28 -0700

    added routes to all html pages

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const index= require('./app/routes/htmlroutes.js');
 
 const PORT = process.env.port || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(index);
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
