72a33a18d6cb34e0b87b4d67e8eaa8d602b06d15
Remeto <remetojohn@gmail.com>
2019-08-17 10:58:32 -0700

    Added array and updated with dummy data for reservations

---
+++ app/data/reservations.js
@@ -0,0 +1,14 @@
+const reservations = [
+    {
+    customerName: "1",
+    phoneNumber: "bbbb",
+    customerEmail: "cccc",
+    customerID: "dddd"
+    },
+    {
+    customerName: "2",
+    phoneNumber: "234123413",
+    customerEmail: "2134132",
+    customerID: "34244" 
+    }
+]
