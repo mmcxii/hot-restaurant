562685186ab5769e0d30a478ba8d20a064033a16
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-08-17 11:54:16 -0700

    API working

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const tableRoute = require('./app/routes/api/tables');
+
 
 const PORT = process.env.port || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api/', tableRoute);
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
-})
+});
